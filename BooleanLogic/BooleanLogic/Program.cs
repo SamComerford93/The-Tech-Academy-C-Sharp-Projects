8dc5db36d555bb434edb0cbb51545b63053e756c
Sam Comerford <samcomerford93@gmail.com>
2024-05-28 20:09:20 +0100

    Boolean logic examples

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND (&&) Operator
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            // OR (||) Operator
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            // EQUAL (==) Operator
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            // NOT EQUAL (!=) Operator
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            // XOR (^) Operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
