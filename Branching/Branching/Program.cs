040cd4c30ca0040e31e53db9b65479d78fbf1276
Sam Comerford <samcomerford93@gmail.com>
2024-05-29 21:10:13 +0100

    Branching examples completed

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            // IF, ELSE IF and ELSE
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+            // Ternary Operators
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+
+
+            // IF, ELSE IF and ELSE with input
+            int roomTemperature = 70;
+
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else if (roomTemperature > currentTemperature)
+            {
+                Console.WriteLine("It is colder than room temperature.");
+            }
+            else
+            {
+                Console.WriteLine("Uhhhhhh..... something went wrong.");
+            }
+
+
+            // Ternary Operator with input
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
