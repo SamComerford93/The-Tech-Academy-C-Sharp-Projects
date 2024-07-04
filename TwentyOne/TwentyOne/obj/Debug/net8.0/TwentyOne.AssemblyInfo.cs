fdfd0a45850ad9dd02919590c7203781b65eac8a
Sam Comerford <samcomerford93@gmail.com>
2024-07-04 21:45:36 +0100

    Moved Shuffle method to the deck class

--- TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
+++ TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9789fcd5310cfe1b6cc19a6a3ef71972dc83f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f330d7571059a27238e952e3115dd9e32b76847")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
