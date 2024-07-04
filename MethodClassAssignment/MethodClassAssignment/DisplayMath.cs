3f330d7571059a27238e952e3115dd9e32b76847
Sam Comerford <samcomerford93@gmail.com>
2024-07-04 21:32:18 +0100

    Method class assignment completed

---
+++ MethodClassAssignment/MethodClassAssignment/DisplayMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class DisplayMath
+    {
+        // Create a void method that does a math operation to the first integer and displays the second
+        public void TwoNums(int num1, int num2)
+        {
+            // Added 10 to the first intger passed in
+            num1 = num1 + 10;
+            // Displayed the second integer passed in
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            DisplayMath numbers = new DisplayMath();
+
+            // Call the method passing in two numbers
+            numbers.TwoNums(12, 15);
+
+            // Call the method specifying the parameters by name
+            numbers.TwoNums(num1: 20, num2: 30);
+        }
+    }
+}
