ebd2b3eb5201e1cae27598aa61f22340650bb39e
Sam Comerford <samcomerford93@gmail.com>
2024-07-04 20:54:51 +0100

    Main method assignment completed

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathOperations
+    {
+        // Create a method that takes in an integer
+        public int ChangeNum(int num)
+        {
+            return num + 8;
+        }
+
+        // Create a method that takes in a decimal
+        public int ChangeNum(decimal num)
+        {
+            // Convert the decimal into a int
+            int wholeNum = Convert.ToInt32(num);
+            return wholeNum * 10;
+        }
+        
+        // Create a method that takes in a string
+        public int ChangeNum(string num)
+        {
+            // Convert string to int 
+            int wholeNum = Convert.ToInt32(num);
+            return wholeNum - 13;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperations math1 = new MathOperations();
+
+            // Call method passing in an integer
+            Console.WriteLine(math1.ChangeNum(34));
+
+            // Call method passing in a decimal 
+            Console.WriteLine(math1.ChangeNum(14.7m));
+
+            // Call method passing in a string
+            Console.WriteLine(math1.ChangeNum("16"));
+        }
+    }
+}
