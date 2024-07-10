1dfd95317f225083467c11f72828ee5abc5bdb31
Sam Comerford <samcomerford93@gmail.com>
2024-07-10 15:49:10 +0100

    Created some overloaded operator methods and tested using them

--- TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
+++ TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a79cdf9c4e22d0f56aedede6fc90e035c1e57bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bac10cdf781151922eb8e0385431f21c76af634")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
