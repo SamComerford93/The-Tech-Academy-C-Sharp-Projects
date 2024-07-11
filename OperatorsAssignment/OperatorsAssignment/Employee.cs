87d08077d8993d81bef0159b22e10e3f411d1e26
Sam Comerford <samcomerford93@gmail.com>
2024-07-11 20:48:31 +0100

    Operators assignment completed

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    // Create an Employee class
+    public class Employee
+    {
+        // Create properties Id, FirstName and LastName
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload == operator to compare if two Id's are the same
+        public static bool operator == (Employee left, Employee right)
+        {
+            return (left.Id == right.Id);
+        }
+
+        // Overload != operator to compare if two Id's are the same
+        public static bool operator != (Employee left, Employee right)
+        {
+            return (left.Id != right.Id);
+        }
+
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating 3 employee objects to test methods
+            Employee person1 = new Employee() { Id = 10, FirstName = "Sam", LastName = "Johnson" };
+            Employee person2 = new Employee() { Id = 11, FirstName = "Bob", LastName = "Smith" };
+            Employee person3 = new Employee() { Id = 11, FirstName = "Bob", LastName = "Smith" };
+
+            // Test with differnt Id values
+            Console.WriteLine(person1 == person2);
+            Console.WriteLine(person1 != person2);
+
+            // Test with same Id values
+            Console.WriteLine(person2 == person3);
+            Console.WriteLine(person2 != person3);
+        }
+    }
+}
