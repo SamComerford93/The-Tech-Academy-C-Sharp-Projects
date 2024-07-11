c7d6961a1227fed93bce9e338a6e2fccf914bad9
Sam Comerford <samcomerford93@gmail.com>
2024-07-11 21:46:50 +0100

    Parameters assignment completed

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // Make an Emplyee class take a generic type parameter 
+    public class Employee<T>
+    {
+        // Add property called things with generic list data type
+        public List<T> things { get; set; }
+
+
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Employee with type string
+            Employee<string> employee1 = new Employee<string>();
+            employee1.things = new List<string>();
+            // Assign a list of stings as the property value 
+            employee1.things.Add("Uniform"); 
+            employee1.things.Add("Tools");
+            employee1.things.Add("Laptop");
+
+            // Print list to console
+            foreach (string item  in employee1.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Instantiate Employee with type int
+            Employee<int> employee2 = new Employee<int>();
+            employee2.things = new List<int>();
+            // Assign a list of integers as the property value 
+            employee2.things.Add(12);
+            employee2.things.Add(4);
+            employee2.things.Add(1993);
+
+            // Print list to console
+            foreach (int item in employee2.things)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
