34466def605a8a978c0d2aaefa0c5bfa50464602
Sam Comerford <samcomerford93@gmail.com>
2024-07-18 20:59:11 +0100

    DateTime assignment completed

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the current date and time to the console
+            Console.WriteLine("The current date and time is {0}", DateTime.Now);
+
+            // Ask the user to input a number
+            Console.WriteLine("Please enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Print the exact time it will be in (num) hours
+            Console.WriteLine("The date and time in {0} hours will be {1}", num, DateTime.Now.AddHours(num));
+
+        }
+    }
+}
